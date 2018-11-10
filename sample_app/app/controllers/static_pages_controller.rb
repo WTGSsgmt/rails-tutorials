bd7981bd0c7d85bd79df346c86320c2abd2b6585
keisuke shigematsu <keisuke.shigematsu@bizreach.co.jp>
2018-11-10 20:17:51 +0900

    make static pages test

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -4,4 +4,17 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
 end
+
+# 純粋なRuby言語であれば、これらのメソッドは何も実行しません。
+# しかし、Railsでは動作が異なります。StaticPagesControllerはRubyのクラスですが、ApplicationControllerクラスを継承しているため、
+# StaticPagesControllerのメソッドは (たとえ何も書かれていなくても) Rails特有の振る舞いをします。
+# 具体的には、/static_pages/homeというURLにアクセスすると、RailsはStaticPagesコントローラを参照し、homeアクションに記述されているコードを実行します。
+# その後、そのアクションに対応するビュー (1.3.3で説明したMVCのVに相当) を出力します。
+# 今回の場合、homeアクションが空になっているので、/static_pages/homeにアクセスしても、単に対応するビューが出力されるだけです。
--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,45 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  # setupという特別なメソッド (各テストが実行される直前で実行されるメソッド)
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  # 言葉で表すと
+  # 「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ。そうすれば、リクエストに対するレスポンスは[成功]になるはず。」となります。
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
+# テストの実行にはある程度時間がかかります。これには２つの要因が絡んでいます。
+# (1) Springサーバーを起動してRails環境を事前読み込みするのに時間がかかる。ただしこれは最初の1回だけです。
+# (2) Rubyそのものの起動に時間がかかる (2番目の要因については、3.6.2で紹介するGuardを導入することで改善できます)。
+#
