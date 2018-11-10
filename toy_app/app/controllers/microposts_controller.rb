0135c68781a57278d0abf6403c925afbb4336581
keisuke shigematsu <keisuke.shigematsu@bizreach.co.jp>
2018-11-10 16:49:06 +0900

    tes

--- toy_app/app/controllers/microposts_controller.rb
+++ toy_app/app/controllers/microposts_controller.rb
@@ -28,11 +28,11 @@ class MicropostsController < ApplicationController
 
     respond_to do |format|
       if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost まいくろぽすと was successfully created.' }
-        format.json { render :show, status: :created, location: @micropost }
+        format.html {redirect_to @micropost, notice: 'Micropost まいくろぽすと was successfully created.'}
+        format.json {render :show, status: :created, location: @micropost}
       else
-        format.html { render :new }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @micropost.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +42,11 @@ class MicropostsController < ApplicationController
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
-        format.html { redirect_to @micropost, notice: 'Micropost まいくろぽすと was successfully updated.' }
-        format.json { render :show, status: :ok, location: @micropost }
+        format.html {redirect_to @micropost, notice: 'Micropost まいくろぽすと was successfully updated.'}
+        format.json {render :show, status: :ok, location: @micropost}
       else
-        format.html { render :edit }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @micropost.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,19 +56,20 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     respond_to do |format|
-      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to microposts_url, notice: 'Micropost was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
+  end
 end
